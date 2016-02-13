2556407a5698bb30b5f35c158837d3a93b4f3c94
Nikolay Sidoruk <nnsidoruk@gmail.com>
2016-02-13 16:12:52 +0300

    Few fields added

--- lib/i18n/patch/watcher_patch.rb
+++ lib/i18n/patch/watcher_patch.rb
@@ -1,10 +1,10 @@
 [  
-  [RPG::Actor, %w(name nickname)],
+  [RPG::Actor, %w(name nickname description)],
   [RPG::Class, %w(name)],
-  [RPG::Skill, %w(name description)],
+  [RPG::Skill, %w(name description message1)],
   [RPG::Enemy, %w(name)],
   [RPG::Item, %w(name description)],
   [RPG::Weapon, %w(name description)],
   [RPG::Armor, %w(name description)],
-  [RPG::State, %w(name)]
+  [RPG::State, %w(name message1 message2 message3 message4)]
 ].each { |arr| I18n::DatabaseTranslator.watch(*arr) }
